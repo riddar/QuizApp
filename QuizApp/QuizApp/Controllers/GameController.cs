ff946393995490bd7d73021b6057dcf8b1259b03
Fredrik Ridderheim <fredrik.ridderheim@hotmail.com>
2018-05-03 12:51:52 +0200

    Added GameController & Quiz View

---
+++ QuizApp/QuizApp/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizApp.Data;
+using QuizApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public GameController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+    }
+}
