ddc0fcfcf9b1034e468fe46cfee7de6dcd59fe93
Fredrik Ridderheim <fredrik.ridderheim@hotmail.com>
2018-05-04 10:38:21 +0200

    fixed login

--- QuizApp/QuizApp/Migrations/20180504083730_init.Designer.cs
+++ QuizApp/QuizApp/Migrations/20180504083730_init.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace QuizApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180504083730_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
